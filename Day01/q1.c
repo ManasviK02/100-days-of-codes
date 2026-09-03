3043e7ec8ca4cb6c88cf89a41beaf468f71151b7
ManasviK02 <Manasvi.41297@stu.upes.ac.in>
2026-09-03 14:22:09 +0000

    Day 01

---
+++ Day01/q1.c
@@ -0,0 +1,22 @@
+/*
+ * Name : Manasvi
+ * Roll : 590041297
+ * Day : 01 Question: 1
+ * Date : 03-09-2026
+ *
+ * PROBLEM STATEMENT:
+ * Write a program to input two numbers and display their sum.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    printf("Sum = %d\n", a + b);
+
+    return 0;
+}
---
+++ Day01/q2.c
@@ -0,0 +1,24 @@
+/*
+ * Name : Manasvi
+ * Roll : 590041297
+ * Day : 01 Question: 2
+ * Date : 03-09-2026
+ *
+ * PROBLEM STATEMENT:
+ * Write a program to input two numbers and display their
+ * sum, difference, product, and quotient.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    printf("Sum=%d, Diff=%d, Product=%d, Quotient=%d\n",
+           a + b, a - b, a * b, a / b);
+
+    return 0;
+}
